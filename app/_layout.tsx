4a44582d30df44b40489f06de77e79a0b65b1f6d
Alberto Anunciado Jr <92003785+HYSAA@users.noreply.github.com>
2025-02-17 18:52:27 +0800

    added teamMember and Display

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -21,6 +21,10 @@ export default function RootLayout() {
       <Stack.Screen name="library" options={{headerShown: false}} />           {/*library screen*/}
       <Stack.Screen name="second_library" options={{headerShown: false}} />    {/*second_library screen*/}
       <Stack.Screen name="camera" options={{headerShown: false}} />
+      <Stack.Screen name="viewProcedure" options={{headerShown: false}} />
+      <Stack.Screen name="addTeamMember" options={{headerShown: false}} />
+      <Stack.Screen name="addTeamMemberCode" options={{headerShown: false}} />
+      <Stack.Screen name="addTeamMemberDisplay" options={{headerShown: false}} />
     </Stack>
     </AuthProvider>
     
